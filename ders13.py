41790a6ae49abb81ce9564fc514dcf09c2a22a82
KantoshK <110815289+KantoshK@users.noreply.github.com>
2023-03-23 14:17:32 +0300

    Add files via upload

---
+++ ders13.py
@@ -0,0 +1,52 @@
+# FOR DÖNGÜSÜ
+# listedeki elemanları sıralamayı sağlar.
+
+# list
+# meyvelerList = ['elma', 'armut', 'kavun', 'karpuz']
+# for meyve in meyvelerList:
+#     print(meyve)
+
+# tuple
+# meyvelerTuple = ('elma', 'armut', 'kavun', 'karpuz')
+# for meyve in meyvelerTuple:
+#     print(meyve)
+
+# set
+# meyvelerSet = {'elma', 'armut', 'kavun', 'karpuz'}
+# for meyve in meyvelerSet:
+#     print(meyve)
+
+# kisiselDic = {'isim': 'ad',
+#               'yaş': '10',
+#               'meslek': 'iş'
+#               }
+# for bilgi in kisiselDic:
+#     print(bilgi)    # burada sadece keyleri yazdı.
+# for bilgi in kisiselDic.values():
+#     print(bilgi)    # değişkendeki değerleri yazdı.
+# for bilgi in kisiselDic.items():
+#     print(bilgi)    # değişkendeki itemleri yazdı.
+
+# for keys,values in kisiselDic.items():
+#     print(keys)
+#     print(values)
+
+# strVal = "Merhaba Python"
+# for harf in strVal:
+#     print(harf)
+
+# list içine tuple değişkenleri atayalım.
+# listVal = [(1, 2, 3), ('elma', 'armut', 'kavun'), ('a', 'b', 'c')]
+# print(type(listVal))
+
+# for val in listVal:
+#     print(val)
+#     print(type(val))
+
+# for val1,val2,val3 in listVal:
+#     print(val1)
+#     print(type(val1))
+#     print(val2)
+#     print(type(val2))
+#     print(val3)
+#     print(type(val3))
