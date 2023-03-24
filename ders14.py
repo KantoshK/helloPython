bb72d924123be48459d7bbd9676a9b69bc7dbc0f
KantoshK <110815289+KantoshK@users.noreply.github.com>
2023-03-24 19:22:58 +0300

    Add files via upload

---
+++ ders14.py
@@ -0,0 +1,37 @@
+# RANGE
+# range(10)   # 0' dan 10 kadar (10 dahil değil) sayılar
+# print(type(range))
+
+# rangeVal = range(10)
+# print(rangeVal)
+# print(type(rangeVal))
+# print(*rangeVal)    # başına * koyduğumuzda belirlenen sayıları yazdı.
+
+# intVal = 0
+# while intVal < 10:
+#     print(intVal)
+#     intVal += 1
+
+# listSayi = ['0','1','2','3','4','5','6','7','8','9']
+# for val in listSayi:
+#     print(val)
+
+# for val in range(10):
+#     print(val)
+
+# for val in range(10,30,2):  # 10'dan başlayıp 30'a kadar ikişer ikişer yazdırır.
+#     print(val)
+
+# piramit yapımı
+# for x in range(0,11):
+#     print('*' * x)
+# for x in range(9,0,-1):
+#     print('*' * x)
+
+# val = 0
+# while val < 10:
+#     for x in range(0,11):
+#         print('*' * x)
+#     for x in range(9,0,-1):
+#         print('*' * x)
+#     val += 1
