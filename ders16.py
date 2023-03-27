569c7c888aa9cc10f406a441a97a90581a9d5a31
KantoshK <110815289+KantoshK@users.noreply.github.com>
2023-03-27 21:33:09 +0300

    Add files via upload

---
+++ ders16.py
@@ -0,0 +1,31 @@
+# BREAK AND CONTINUE
+# döngüye belli yerlerde müdahale etmeyi sağlar.
+
+# bu kodda break, elemanlar listelenirken sıra 'kavun'a geldiğinde döngüyü bitirir.
+# meyvelerList = ['elma', 'armut', 'kavun', 'karpuz']
+# for meyve in meyvelerList:
+#     if meyve == 'kavun':
+#         break
+#     print(meyve)
+# print("döngü sonlandırıldı")
+
+# bu kodda continue, elemanlar listelenirken sıra 'kavun'a geldiğinde orayı atlar ve kaldığı yerden, döngünün başından devam eder.
+# meyvelerList = ['elma', 'armut', 'kavun', 'karpuz']
+# for meyve in meyvelerList:
+#     if meyve == 'kavun':
+#         continue
+#     print(meyve)
+# print("döngü sonlandırıldı")
+
+# while True:
+#     sayi = int(input("10'dan büyük bir sayı giriniz: "))
+
+    # if sayi > 10:
+    #     print("Doğru!")
+    #     print("Girilen sayı:",sayi)
+    #     continue
+    # if sayi <= 10:
+    #     print("Yanlış!")
+    #     print("Girilen sayı:", sayi)
+    #     break
+# print("döngü sonlandırıldı")
