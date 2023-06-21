6f048f2ec4de46ebe2448570cf320f2ab75d5707
Kağan Kaçar <110815289+KantoshK@users.noreply.github.com>
2023-06-21 07:46:46 +0300

    Add files via upload

---
+++ ders21.py
@@ -0,0 +1,34 @@
+# RESCURSIVE
+# Rescursive: Kendi kendini çağıran fonksiyonlardır.
+
+# def func1():
+#     return 1
+# def func2():
+#     return 2
+# def func():
+#     print(func1() + func2())
+# func()
+
+# num = 0
+# def func():
+#     global num
+#     print(num)
+#     num += 1
+#     func()
+# func()
+
+# def fact(num):
+#     if num > 1:
+#         return fact(num-1) * num
+#     else:
+#         return 1
+# print(fact(5))
+
+# def fact(num):
+#     if num > 1:
+#         sonuc = fact(num-1) * num
+#         print(sonuc)
+#         return sonuc
+#     else:
+#         return 1
+# fact(5)
